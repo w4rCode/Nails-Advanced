e7e4ae56c813705c4e1a01852d0ce5da722e9485
Ana Gabriely <84465419+w4rCode@users.noreply.github.com>
2023-10-04 13:44:08 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+
+  $('.carrosel').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+
+    responsive: [
+    
+        {
+          breakpoint: 1200,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 740,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
+  });
+
+
+
+
+  var acc = document.getElementsByClassName("accordion");
+  var i;
+  
+  for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+  
+      this.classList.toggle("active");
+  
+      var panel = this.nextElementSibling;
+      if (panel.style.display === "block") {
+        panel.style.display = "none";
+      } else {
+        panel.style.display = "block";
+      }
+    });
+  }
